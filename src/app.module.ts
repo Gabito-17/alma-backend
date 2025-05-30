85f918a4e58ff65a21785e03b528787cb6df3cc1
XGabito12 <juangabrielpavon@gmail.com>
2025-05-30 10:52:02 -0300

    Cambio en variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { UsersModule } from './users/users.module';
       type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT ?? '5432'),
-      username: process.env.DB_USERNAME,
+      username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
